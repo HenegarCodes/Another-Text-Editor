e5303ad27677bcbcc85a7160e240e19a71a8b000
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-01-17 16:18:03 -0700

    webpack is complete

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -56,8 +56,24 @@ module.exports = () => {
     
 
     module: {
+      //css
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // babel-loadefr
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
