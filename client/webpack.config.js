a40ea6b48d6e81dc60e6848c56f2287a71b00b3f
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-01-17 15:58:55 -0700

    adding almsot all of the plugins. just need rules for modules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,8 +22,38 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //need webpack
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Another Text Editor',
+        short_name: 'JATE',
+        description: 'Another Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
     ],
+    
+    
 
     module: {
       rules: [
